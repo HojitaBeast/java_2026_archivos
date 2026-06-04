9ef7b5c524bdde6c7eb69997ec0ddb6b0a277d92
HojitaBeast <deker2015@outlook.es>
2026-06-04 15:49:18 -0500

    Agregando la carpeta Bucles con su clase bucle

---
+++ src/bucles/bucle.java
@@ -0,0 +1,12 @@
+package bucles;
+
+public class bucle {
+    public static void main(String[] args) {
+
+        int contador = 10;
+        while (contador >= 0){
+            System.out.println(contador);
+            contador--;
+        }
+    }
+}
