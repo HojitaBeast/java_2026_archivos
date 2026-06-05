0c139b2b9295b67cefe46cccb951e064fc5e93c1
HojitaBeast <deker2015@outlook.es>
2026-06-05 15:24:55 -0500

    Usando el || en una ejemplo

---
+++ src/bucles/Ejemplo01.java
@@ -0,0 +1,25 @@
+package bucles;
+
+import java.util.Scanner;
+
+public class Ejemplo01 {
+    public static void main(String[] args) {
+        Scanner entradaDatos = new Scanner(System.in);
+        String nombreUsuario = "";
+        String clave = "";
+        while (!nombreUsuario.equals("Juan") || !clave.equals("1539")){
+            System.out.println("Ingrese su nombre de usuario");
+            nombreUsuario = entradaDatos.nextLine();
+            System.out.println("Ingrese su clave");
+            clave = entradaDatos.nextLine();
+
+            if (!nombreUsuario.equals("Juan") || !clave.equals("1539")){
+                System.out.println("Datos incorrectos");
+            }
+        }
+        System.out.println("Bienbenido al sistema");
+
+
+        entradaDatos.close();
+    }
+}
