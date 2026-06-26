e2d6d989511ff697986d03a8c963ca0140fd05d8
HojitaBeast <deker2015@outlook.es>
2026-06-26 14:50:30 -0500

    listas

--- src/estructura_datos/arreglos_02.java
+++ src/estructura_datos/arreglos_02.java
@@ -1,6 +1,9 @@
 package estructura_datos;
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
 public class arreglos_02 {
     public static void main(String[] args) {
         //creando mi arreglo de numeros
@@ -15,15 +18,31 @@ public class arreglos_02 {
         numeros[4] = 20;
         numeros[5] = 12;
         System.out.println(Arrays.toString(numeros));
+
         //arreglo con inicializacion
         int[] notas = {10,18,13,15,20,16,18,14,12,12};
         //System.out.println(Arrays.toString(notas));
         for (int i = 0; i < notas.length; i++){
             System.out.println(notas[i]);
         }
-
+        for (int i = 0; i <= notas.length; i++){
+            System.out.println(Arrays.toString(notas));
+        }
         String[] frutas = {"platano","pera","manzana","duraszno","fresas"};
         System.out.println(Arrays.toString(frutas));
+        /*
+        list<TipoDtato> nombreLista = new arregloLista
+         */
+        List<String> fruits= new ArrayList<>();
+        fruits.add("Piña");
+        fruits.add("fresas");
+        fruits.add("platano");
+        fruits.add("mango");
+        fruits.add("durazno");
+        for (int i = 0 ; i <= fruits.size();i++){
+            System.out.println(fruits.get(i));
+        }
+
 
     }
 }
